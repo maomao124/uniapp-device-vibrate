ccf0fbdbd968ace3a425c855e31e6cbea4f2c014
mao <1296193245@qq.com>
2024-02-05 14:49:04 +0800

    uniapp-device-vibrate

---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,54 @@
+<template>
+	<view>
+		<button type="primary" @click="button1" >振动</button>
+		<button type="primary" @click="button2" >振动 - 长</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			button1()
+			{
+				uni.vibrate({
+					success: () => {
+						uni.showToast({
+							title:'成功'
+						})
+					},
+					fail: () => {
+						uni.showToast({
+							title:'失败'
+						})
+					}
+				})
+			},
+			button2()
+			{
+				uni.vibrateLong({
+					success: () => {
+						uni.showToast({
+							title:'成功'
+						})
+					},
+					fail: () => {
+						uni.showToast({
+							title:'失败'
+						})
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 5px;
+}
+</style>
